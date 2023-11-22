77b688f50e9075ac55778a5d646bc5361c417833
Felipe Ávila <79549074+FelipeGFA@users.noreply.github.com>
2023-11-22 12:34:54 -0300

    ProjetoGymSystem

---
+++ admin/actions/count-attendance.php
@@ -0,0 +1,18 @@
+<?php
+
+$servername="sql311.infinityfree.com";
+$uname="if0_35477637";
+$pass="ge5mn6bf5hzDu";
+$db="gymnsb";
+
+$conn=mysqli_connect($servername,$uname,$pass,$db);
+
+if(!$conn){
+    die("Connection Failed");
+}
+
+$sql = "SELECT * FROM attendance";
+                $query = $conn->query($sql);
+
+                echo "$query->num_rows";
+?><!-- Visit codeastro.com for more projects -->
---
+++ customer/pages/actions/dbcon.php
@@ -0,0 +1,9 @@
+<?php
+$con = mysqli_connect("sql311.infinityfree.com","if0_35477637","ge5mn6bf5hzDu","gymnsb");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
